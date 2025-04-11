2446c7b439d214d60b190c16b5d8fca97793e828
Szymutx <szymonpalka56@gmail.com>
2025-04-12 01:12:45 +0200

    Navigation

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,11 +1,13 @@
-console.log("skrypt")
 const menuIcon = document.querySelector(".mobile-icons");
 const menu = document.querySelector("#mobile-menu");
-
+const searchBar = document.querySelector(".search-bar");
+const searchIcon = document.querySelector(".fa-magnifying-glass");
 
 menuIcon.addEventListener("click", () => {
 
     menu.classList.toggle("show-element");
+    searchBar.classList.toggle("hide-element");
+    searchIcon.classList.toggle("hide-element");
 
 });
 
