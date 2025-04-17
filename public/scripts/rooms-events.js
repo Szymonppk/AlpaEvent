066593f0f2fbe12268ed7ac13df11916c01fd5f4
Szymutx <szymonpalka56@gmail.com>
2025-04-18 01:16:04 +0200

    User rooms panel 2

---
+++ public/scripts/rooms-events.js
@@ -0,0 +1,11 @@
+const menuIcon = document.querySelector(".mobile-icons");
+const menu = document.querySelector("#mobile-menu");
+
+menuIcon.addEventListener("click", () => {
+
+    menu.classList.toggle("show-element");
+
+
+});
+
+
